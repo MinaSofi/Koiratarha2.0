f18a1081edc2bc34d1c6eed335843f4d61b8fb6e
Katja Peltonen <katja.peltonen3@metropolia.fi>
2023-10-09 12:55:37 +0300

    prettier

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,47 +1,45 @@
 import app from '../src/app';
-import { postUser } from './userFunctions';
-import { UserTest } from '../src/interfaces/User';
+import {postUser} from './userFunctions';
+import {UserTest} from '../src/interfaces/User';
 import mongoose from 'mongoose';
-import { getNotFound } from './testFunctions';
+import {getNotFound} from './testFunctions';
 import LoginMessageResponse from '../src/interfaces/LoginMessageResponse';
 import randomstring from 'randomstring';
 
-
-
 describe('Testing graphql api', () => {
-    beforeAll(async () => {
-        await mongoose.connect(process.env.DATABASE_URL as string);
-    });
-    
-    afterAll(async () => {
-        await mongoose.connection.close();
-    });
+  beforeAll(async () => {
+    await mongoose.connect(process.env.DATABASE_URL as string);
+  });
 
-    // test not found
-    it('responds with a not found message', async () => {
-        await getNotFound(app);
-    });
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
 
-    let userData: LoginMessageResponse;
-    let userData2: LoginMessageResponse;
-    let adminData: LoginMessageResponse;
+  // test not found
+  it('responds with a not found message', async () => {
+    await getNotFound(app);
+  });
 
-    const testUser: UserTest = {
-        username: 'Test User ' + randomstring.generate(7),
-        password: 'testpassword',
-    };
+  let userData: LoginMessageResponse;
+  let userData2: LoginMessageResponse;
+  let adminData: LoginMessageResponse;
 
-    const testUser2: UserTest = {
-        username: 'Test User ' + randomstring.generate(7),
-        password: 'testpassword',
-    };
+  const testUser: UserTest = {
+    username: 'Test User ' + randomstring.generate(7),
+    password: 'testpassword',
+  };
 
-    const adminUser: UserTest = {
-        username: 'Test User ' + randomstring.generate(7),
-        password: '12345',
-    };
+  const testUser2: UserTest = {
+    username: 'Test User ' + randomstring.generate(7),
+    password: 'testpassword',
+  };
 
-    it('should create a new user', async () => {
-        await postUser(app, testUser);
-    });
-})
+  const adminUser: UserTest = {
+    username: 'Test User ' + randomstring.generate(7),
+    password: '12345',
+  };
+
+  it('should create a new user', async () => {
+    await postUser(app, testUser);
+  });
+});
--- test/userFunctions.ts
+++ test/userFunctions.ts
@@ -1,17 +1,17 @@
-import { UserTest } from "../src/interfaces/User";
+import {UserTest} from '../src/interfaces/User';
 import request from 'supertest';
 import expect from 'expect';
 
 const postUser = (
-    url: string | Function,
-    user: UserTest
+  url: string | Function,
+  user: UserTest
 ): Promise<UserTest> => {
-    return new Promise((resolve, reject) => {
-        request(url)
-            .post('/graphql')
-            .set('Content-type', 'application/json')
-            .send({
-                query: `mutation CreateUser($user: UserInput!) {
+  return new Promise((resolve, reject) => {
+    request(url)
+      .post('/graphql')
+      .set('Content-type', 'application/json')
+      .send({
+        query: `mutation CreateUser($user: UserInput!) {
                     createUser(user: $user) {
                         message
                         data {
@@ -20,28 +20,26 @@ const postUser = (
                         }
                     }
                 }`,
-                variables: {
-                    user: {
-                        username: user.username,
-                        password: user.password,
-                    },
-                },
-            })
-            .expect(200, (err, response) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    const userData = response.body.data.createUser;
-                    expect(userData).toHaveProperty('message');
-                    expect(userData).toHaveProperty('data');
-                    expect(userData.data).toHaveProperty('id');
-                    expect(userData.data.username).toBe(user.username);
-                    resolve(response.body.data.createUser);
-                }
-            });
-    });
+        variables: {
+          user: {
+            username: user.username,
+            password: user.password,
+          },
+        },
+      })
+      .expect(200, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          const userData = response.body.data.createUser;
+          expect(userData).toHaveProperty('message');
+          expect(userData).toHaveProperty('data');
+          expect(userData.data).toHaveProperty('id');
+          expect(userData.data.username).toBe(user.username);
+          resolve(response.body.data.createUser);
+        }
+      });
+  });
 };
 
-export {
-    postUser
-};
+export {postUser};
